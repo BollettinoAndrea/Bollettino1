b57889e40272a82cb876779709d9f2648faaae78
bollettino.20159 <bollettinoandrea06@gmail.com>
2024-09-16 13:11:21 +0200

    commit2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
         System.out.printf("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
-            System.out.println("AIUTO");
+            System.out.println("AIUTO,");
+            System.out.println("PERFAVORE");
         }
 
     }
