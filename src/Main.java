e9dba66a8785c83dacf7f4aca8a11b43db769c07
Bollettino.20159 <bollettinoandrea06@gmail.com>
2024-09-20 09:41:02 +0200

    commit5

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Scanner in = new Scanner(System.in);
+        int auto;
+        String[] marche = new String[auto];
+        String[] modelllo = new String[auto];
+        double[] prezzo = new double[auto];
+        System.out.println("quante auto voi inserire?");
+        auto = in.nextInt();
 
+        do {
+            System.out.println("1. Aggiungi una macchina");
+            System.out.println("2. Visualizza le auto che hai inserito");
+            System.out.println("3. Cerca un'auto inserendo marca e modello");
+            System.out.println("4. Modifica i dati di un auto");
+            System.out.println("5. Cancella un'auto");
+            System.out.println("6. Visualizza le auto in ordine crescente.");
+            System.out.println("7. Visualizza tutti i modelli disel.");
+            System.out.println("8. Esci.");
+        } while ();
     }
+
 }
